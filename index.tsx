95027bcd51f98128022eef6aaebbb67a48d854a6
vinixz121 <viniciusff3058@gmail.com>
2025-09-29 16:23:41 -0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+      <h1>Sistema ERP - Deploy Vercel OK 🚀</h1>
+    </div>
+  );
+}
